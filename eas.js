d673d5c9140d8f53125e12b7977d2f16d193605e
bentleylewis <bbentleylewis@gmail.com>
2025-04-02 16:44:35 -0500

    create functionality for penup/pendown drawing

--- eas.js
+++ eas.js
@@ -1,13 +1,33 @@
 const container = document.getElementById("container");
-
-
+let pendown = false;
 
 function generateGrid() {
     for (let i = 0; i < 256; i++) {
       const gridSquare = document.createElement("div");
       gridSquare.classList.add("grid-square");
       container.appendChild(gridSquare);
+      
+      gridSquare.addEventListener("pointerdown", () => {
+        pendown = true;
+        gridSquare.classList.add("active");
+      });
+
+
+      gridSquare.addEventListener("mouseover", () =>{
+        if (pendown) {
+          gridSquare.classList.add("active");
+        }
+      });
+
+      gridSquare.addEventListener("dragstart", (e) => {
+        e.preventDefault();
+    });
+    
     }
 }
+
+document.addEventListener("pointerup", () => {
+        pendown = false;
+      })
 
 generateGrid();
